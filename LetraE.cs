31cef04baef5fa4138d9af8526e96044219d7d0c
Billy Vanegas <billyvb2004@hotmail.com>
2020-11-04 16:25:32 +0100

    Añadir letra E

---
+++ LetraE.cs
@@ -0,0 +1,18 @@
+//LetraE.cs
+namespace LetrasPOO
+{
+    public sealed class letraE : Letra
+    {
+        public letraE()
+        {
+            base.Nombre = "E";
+            base.Matriz[0] = "*******";
+            base.Matriz[1] = "*      ";
+            base.Matriz[2] = "*      ";
+            base.Matriz[3] = "*******";
+            base.Matriz[4] = "*      ";
+            base.Matriz[5] = "*      ";
+            base.Matriz[6] = "*******";
+        }
+    }
+}
