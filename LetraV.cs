a1930b9a10c5663dcd754277c718759a506122e4
Javier Moreno <mq2880@gmail.com>
2020-11-05 09:37:11 +0100

    Añadidas letras V, Z., Ñ, por Fco Javier Moreno

---
+++ LetraV.cs
@@ -0,0 +1,17 @@
+﻿namespace LetrasPOO
+{
+    public sealed class letraV : Letra
+    {
+        public letraV()
+        {
+            Nombre = "V";
+            Matriz[0] = "*     *";
+            Matriz[1] = "*     *";
+            Matriz[2] = " *   * ";
+            Matriz[3] = " *   * ";
+            Matriz[4] = "  * *  ";
+            Matriz[5] = "  * *  ";
+            Matriz[6] = "   *   ";            
+        }
+    }
+}
