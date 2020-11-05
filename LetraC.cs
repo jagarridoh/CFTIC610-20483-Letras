2e9d81d32d2bf144ddab5be217957cd7388712b1
Sergio de Vega <sergiodevega@hotmail.com>
2020-11-05 11:36:06 +0100

    Letras C y O

---
+++ LetraC.cs
@@ -0,0 +1,17 @@
+namespace LetrasPOO
+{
+    public sealed class letraC : Letra
+    {
+        public letraC()
+        {
+            Nombre = "C";
+            Matriz[0] = "  **** ";
+            Matriz[1] = " *    *";
+            Matriz[2] = "*      ";
+            Matriz[3] = "*      ";
+            Matriz[4] = "*      ";
+            Matriz[5] = " *    *";
+            Matriz[6] = "  **** ";
+        }
+    }
+}
---
+++ LetraO.cs
@@ -0,0 +1,17 @@
+namespace LetrasPOO
+{
+    public sealed class letraO : Letra
+    {
+        public letraO()
+        {
+            Nombre = "O";
+            Matriz[0] = "  ***  ";
+            Matriz[1] = " *   * ";
+            Matriz[2] = "*     *";
+            Matriz[3] = "*     *";
+            Matriz[4] = "*     *";
+            Matriz[5] = " *   * ";
+            Matriz[6] = "  ***  ";
+        }
+    }
+}
