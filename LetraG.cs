95e2072781d49daa8d12a76fe3c4814d7230b87d
Carola Casais <carola.casais@gmail.com>
2020-11-06 20:55:51 +0100

    Letras F, G y H

---
+++ LetraG.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LetrasPOO
+{
+    public sealed class letraG : Letra
+    {
+        public letraG()
+        {
+            base.Nombre ="G";
+            base.Matriz [0] = "*******";
+            base.Matriz [1] = "*      ";
+            base.Matriz [2] = "*      ";
+            base.Matriz [3] = "*"      ;
+            base.Matriz [4] = "*   ***";
+            base.Matriz [5] = "*     *";
+            base.Matriz [6] = "*******";
+        }
+
+    }
+
+}
