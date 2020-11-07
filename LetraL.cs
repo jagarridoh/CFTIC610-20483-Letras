cd9e11f33185a2c6d46319c4156a4589467a7429
JoseVicenteTejero <71097282+JoseVicenteTejero@users.noreply.github.com>
2020-11-07 12:21:45 +0100

    Add files via upload

---
+++ LetraL.cs
@@ -0,0 +1,17 @@
+﻿namespace LetrasPOO
+{
+    public sealed class LetraT : Letra
+    {
+        public LetraL()
+        {
+            Nombre = "L";
+            Matriz[0] = "*      ";
+            Matriz[1] = "*      ";
+            Matriz[2] = "*      ";
+            Matriz[3] = "*      ";
+            Matriz[4] = "*      ";
+            Matriz[5] = "*      ";
+            Matriz[6] = "*******";            
+        }
+    }
+}
