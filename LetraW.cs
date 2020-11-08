93a78167f5906df638f909bb55469acfa90a21dc
Jose Rene Fuentes <jrfuent@gmail.com>
2020-11-09 00:45:15 +0100

    Letras K y W han sido agregadas

---
+++ LetraW.cs
@@ -0,0 +1,17 @@
+namespace LetrasPOO
+{
+    public sealed class LetraW : Letra
+    {
+        public LetraW()
+        {
+            Nombre = "V";
+            Matriz[0] = "*   *   *";
+            Matriz[1] = "*   *   *";
+            Matriz[2] = "*   *   *";
+            Matriz[3] = "*   *   *";
+            Matriz[4] = "*   *   *";
+            Matriz[5] = "* *   * *";
+            Matriz[6] = "*       *";
+        }
+    }
+}
